efbd31b00d32c7aa82849791f6540bdf178ebdb0
Tiago Vitorino <89148458+Vitorino470@users.noreply.github.com>
2025-03-27 22:40:54 +0000

    correção de outro erro na exibição

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -43,7 +43,7 @@ int main() {
 
   //Exibição dos dados da carta 1
   
-  printf("Carta 1\n");
+  printf("Carta 1 \n");
   printf("- Estado: %s \n - Código da Carta: %s \n", estado, codigo); 
   printf("- Nome da Cidade: %s \n - População: %d habitantes \n", nome, populacao);
   printf("- Área em Km²: %.2f Km² \n - PIB: %.2f bilhões de Reais \n", area, pib);
@@ -84,7 +84,7 @@ int main() {
 
  //Exibição dos dados da carta 2
 
- printf("Carta 2\n");
+ printf("Carta 2 \n");
  printf("- Estado: %s \n - Código da Carta: %s \n", estado02, codigo02); 
  printf("- Nome da Cidade: %s \n - População: %d habitantes \n", nome02, populacao02);
  printf("- Área em Km²: %.2f Km² \n - PIB: %.2f bilhões de Reais \n", area02, pib02);
